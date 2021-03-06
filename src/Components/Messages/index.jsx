e0b9a61eccd4f800ece4d8e77c3b58b051a66853
VilizarTodorov <todorovvilizar@gmail.com>
2021-03-06 23:35:03 +0200

    create and get messages

--- src/Components/Messages/index.jsx
+++ src/Components/Messages/index.jsx
@@ -1,4 +1,6 @@
+import { useQuery } from "@apollo/client";
 import styled from "styled-components";
+import { messagesQuery } from "../../Queries";
 import Message from "./Message";
 
 const MessagesWrapper = styled.div`
@@ -6,14 +8,29 @@ const MessagesWrapper = styled.div`
   grid-row: 2;
 `;
 
-const Messages = ({ messages }) => {
+const Messages = ({ channelId }) => {
+  const { loading, error, data } = useQuery(messagesQuery, { variables: { channelId } });
+  if (loading) {
+    return <div>...Loading</div>;
+  }
+
+  if (data) {
+    console.log(channelId);
+    console.log(data);
+    return <div>done</div>;
+  }
+
+  if (error) {
+    console.log(error);
+  }
+
   return (
     <MessagesWrapper>
-      <ul className="messages-list">
+      {/* <ul className="messages-list">
         {messages.map((message, index) => (
           <Message key={index}>{message}</Message>
         ))}
-      </ul>
+      </ul> */}
     </MessagesWrapper>
   );
 };
